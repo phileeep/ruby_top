53db9791737c598f84e2fa7ce8d0808977b8147d
Phil Smithies <philsmithies@gmail.com>
2021-05-01 11:19:20 +0200

    data types

---
+++ 1_data_types/exercises/numbers_exercises.rb
@@ -0,0 +1,35 @@
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def multiply(a, b)
+  a * b
+end
+
+def divide(a, b)
+  a / b
+end
+
+def remainder(a, b)
+  a % b
+end
+
+def float_division(a, b)
+  (a / b).to_f
+end
+
+def string_to_number(string)
+  string.to_i
+end
+
+def even?(number)
+ number.even?
+end
+
+def odd?(number)
+  number.odd?
+end
---
+++ 1_data_types/exercises/string_exercises.rb
@@ -0,0 +1,40 @@
+def concatenate_example(string)
+  # use concatenation to format the result to be "Classic <string>"
+  "Classic " << string
+end
+
+def concatenate(string)
+  "Hello #{string}!"
+end
+
+def substrings(word)
+  word[0..3]
+end
+
+def capitalize(word)
+  word.capitalize
+end
+
+def uppercase(string)
+  string.upcase
+end
+
+def downcase(string)
+  string.downcase!
+end
+
+def empty_string(string)
+  string == "" ? true : false
+end
+
+def string_length(string)
+  string.length
+end
+
+def reverse(string)
+  string.reverse
+end
+
+def space_remover(string)
+ string.gsub(' ', '')
+end
